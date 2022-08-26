dfa629ac30e3c4986d95721880781b86d05215b5
Tomisin <tadereti@gmail.com>
2022-08-26 16:05:34 +0100

    http get

--- Template_Project/Utility/HttpClientRepository.cs
+++ Template_Project/Utility/HttpClientRepository.cs
@@ -2,7 +2,7 @@
 using Newtonsoft.Json;
 using System.Text;
 
-namespace Template_Project.Dao
+namespace Template_Project.Utility
 {
     public class HttpClientRepository
     {
@@ -23,6 +23,18 @@ namespace Template_Project.Dao
             {
                 var contents = BuildReqContent<T>(client, _baseURL, model, PartOfUrl);
                 response = await client.PostAsync(contents.Item1, contents.Item2);
+            }
+            var responseObj = await response.Content.ReadAsStringAsync();
+            return new Tuple<string, HttpResponseMessage>(responseObj, response);
+        }
+
+        public static async Task<Tuple<string, HttpResponseMessage>> GetContentAsync<T>(string _baseURL, T model, string PartOfUrl)
+        {
+            var response = new HttpResponseMessage();
+            using (var client = new HttpClient())
+            {
+                var contents = BuildReqContent<T>(client, _baseURL, model, PartOfUrl);
+                response = await client.GetAsync(contents.Item1);
             }
             var responseObj = await response.Content.ReadAsStringAsync();
             return new Tuple<string, HttpResponseMessage>(responseObj, response);
